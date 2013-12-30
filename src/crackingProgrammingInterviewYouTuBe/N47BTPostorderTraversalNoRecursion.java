cc188ac1f3eb199e31f9ae3a225a0c98b1149a52
John Qiao <qiaoshun8888@gmail.com>
2013-12-30 18:06:24 -0500

    Binary Tree Post-order Traversal without recursion (using stack)
    	/**
    	 * 			  5
    	 * 		  /      \
    	 *       3        6
    	 *      / \      / \
    	 *     2   4    7   8
    	 *        / \  / \
    	 *       9  0 1   11
    	 * 
    	 * Output:  2 9 0 4 3    1 11 7 8 6    5 
    	 * 
    	 * */

--- src/crackingProgrammingInterviewYouTuBe/N47BTPostorderTraversalNoRecursion.java
+++ src/crackingProgrammingInterviewYouTuBe/N47BTPostorderTraversalNoRecursion.java
@@ -1,22 +1,20 @@
 package crackingProgrammingInterviewYouTuBe;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 import java.util.Stack;
 
-public class N45PrintBTBoundary {
-	
+import crackingProgrammingInterviewYouTuBe.N45PrintBTBoundary.Node;
+
+public class N47BTPostorderTraversalNoRecursion {
+
 	/**
-	 * Print a full binary tree's boundary in counterclockwise order
+	 * Binary Tree Post-order Traversal without recursion (using stack)
 	 * 
 	 * */
 	
 	static class Node {
 		Node left, right;
 		int v;
-		public Node (int v) {
+		public Node(int v) {
 			this.v = v;
 		}
 	}
@@ -30,53 +28,62 @@ public class N45PrintBTBoundary {
 	 *        / \  / \
 	 *       9  0 1   11
 	 * 
-	 * Output:  5 3 2 9 0 1 11 8 6 
+	 * Output:  2 9 0 4 3    1 11 7 8 6    5 
 	 * 
 	 * */
-	public static void printBinaryTree(Node root) {
-		Set<Node> set = new HashSet<Node>();
-		// Pre-store the left-most leaves and right-most leaves
-		Node runner = root;
-		while (runner.left != null) {
-			set.add(runner.left);
-			runner = runner.left;
-		}
-		runner = root;
-		while (runner.right != null) {
-			set.add(runner.right);
-			runner = runner.right;
-		}
-		
+	public static void postOrderTraversal(Node root) {
 		Stack<Node> stack = new Stack<Node>();
-		stack.push(root);
-		print(root);
+		Stack<Node> stack2 = new Stack<Node>();
 		
+		stack.push(root.right);
 		while(!stack.isEmpty()) {
 			Node n = stack.pop();
-			if ((set.contains(n) || isLeaf(n)) && n != root.right) {
-				print(n);
-				set.remove(n);
+			stack2.push(n);
+
+			if (n.left != null) {
+				stack.push(n.left);
 			}
 			if (n.right != null) {
 				stack.push(n.right);
 			}
+		}
+		
+		stack.push(root.left);
+		while(!stack.isEmpty()) {
+			Node n = stack.pop();
+			stack2.push(n);
+			
 			if (n.left != null) {
 				stack.push(n.left);
 			}
+			if (n.right != null) {
+				stack.push(n.right);
+			}
 		}
 		
-		print(root.right);
-		
+		while (!stack2.isEmpty()) {
+			print(stack2.pop());
+		}
+		print(root);
 	}
 	private static void print(Node n) {
 		System.out.print(n.v + " ");
 	}
-	private static boolean isLeaf(Node n) {
-		return n.left == null && n.right == null; 
-	}
 	
 	public static void main(String [] args) {
-		 Node root = new Node(5);
+		/**
+		 * 			  5
+		 * 		  /      \
+		 *       3        6
+		 *      / \      / \
+		 *     2   4    7   8
+		 *        / \  / \
+		 *       9  0 1   11
+		 * 
+		 * Output:  2 9 0 4 3 1 11 7 8 6 5 
+		 * 
+		 * */
+		Node root = new Node(5);
 
 		 root.left = new Node(3);
 		 root.left.left = new Node(2);
@@ -90,6 +97,6 @@ public class N45PrintBTBoundary {
 		 root.right.left.right = new Node(11);
 		 root.right.right = new Node(8);
 		 
-		 printBinaryTree(root);
+		 postOrderTraversal(root);
 	}
 }
