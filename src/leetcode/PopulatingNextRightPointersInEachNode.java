b127dac4eec3f2d897ad29233494985324572d6f
John Qiao <qiaoshun8888@gmail.com>
2014-01-04 02:53:34 -0500

    /** 	 * Given a binary tree 	 * 	 * struct TreeLinkNode { TreeLinkNode *left; TreeLinkNode *right; 	 * TreeLinkNode *next; } Populate each next pointer to point to its next 	 * right node. If there is no next right node, the next pointer should be 	 * set to NULL. 	 * 	 * Initially, all next pointers are set to NULL. 	 * 	 * Note: 	 * 	 * You may only use constant extra space. You may assume that it is a 	 * perfect binary tree (ie, all leaves are at the same level, and every 	 * parent has two children). 	 * 	 * For example, Given the following perfect binary tree, 	 * 			     1 		       /  \ 		      2    3 		     / \  / \ 		    4  5  6  7 	 * 	 * After calling your function, the tree should look like: 	 * 			     1 -> NULL 		       /  \ 		      2 -> 3 -> NULL 		     / \  / \ 		    4->5->6->7 -> NULL 	 * 	 * */

---
+++ src/leetcode/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+public class PopulatingNextRightPointersInEachNode {
+
+	/**
+	 * Given a binary tree
+	 * 
+	 * struct TreeLinkNode { TreeLinkNode *left; TreeLinkNode *right;
+	 * TreeLinkNode *next; } Populate each next pointer to point to its next
+	 * right node. If there is no next right node, the next pointer should be
+	 * set to NULL.
+	 * 
+	 * Initially, all next pointers are set to NULL.
+	 * 
+	 * Note:
+	 * 
+	 * You may only use constant extra space. You may assume that it is a
+	 * perfect binary tree (ie, all leaves are at the same level, and every
+	 * parent has two children).
+	 * 
+	 * For example, Given the following perfect binary tree,
+	 * 
+			     1
+		       /  \
+		      2    3
+		     / \  / \
+		    4  5  6  7
+	 * 
+	 * After calling your function, the tree should look like:
+	 * 
+			     1 -> NULL
+		       /  \
+		      2 -> 3 -> NULL
+		     / \  / \
+		    4->5->6->7 -> NULL
+	 * 
+	 * */
+	
+	
+	public class TreeLinkNode {
+		int val;
+		TreeLinkNode left, right, next;
+		TreeLinkNode(int x) { val = x; }
+	}
+
+
+	public void connect(TreeLinkNode root) {
+		TreeLinkNode layer = root;
+		while (layer != null) {
+			TreeLinkNode runner = layer;
+
+			while (runner.next != null && !isLeaf(runner)) {
+				runner.left.next = runner.right;
+				runner.right.next = runner.next.left;
+				runner = runner.next;
+			}
+
+			if (!isLeaf(runner)) {
+				runner.left.next = runner.right;
+			}
+
+			layer = layer.left;
+		}
+	}
+
+	private boolean isLeaf(TreeLinkNode node) {
+		return node.left == null && node.right == null;
+	}
+}
