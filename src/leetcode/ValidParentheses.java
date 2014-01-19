13e47f2fdd3f818d1617cd1f84d8e82945a921f1
John Qiao <qiaoshun8888@gmail.com>
2014-01-19 14:07:32 -0500

    Valid Parentheses
    	/**
    	 * Given a string containing just the characters '(', ')', '{', '}',
    '[' and
    	 * ']', determine if the input string is valid.
    	 * 
    	 * The brackets must close in the correct order, "()" and "()[]{}" are
    all
    	 * valid but "(]" and "([)]" are not.
    	 * 
    	 * */

---
+++ src/leetcode/ValidParentheses.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import java.util.*;
+
+public class ValidParentheses {
+
+	/**
+	 * Given a string containing just the characters '(', ')', '{', '}', '[' and
+	 * ']', determine if the input string is valid.
+	 * 
+	 * The brackets must close in the correct order, "()" and "()[]{}" are all
+	 * valid but "(]" and "([)]" are not.
+	 * 
+	 * */
+	
+	public boolean isValid(String s) {
+        if ((s.length() & 1) > 0) return false; // odd length
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        char [] cc = s.toCharArray();
+        
+        for (char c : cc) {
+        	switch(c) {
+        		case '(':
+        		case '[':
+        		case '{': {
+        			stack.push((int)c);
+        			break;
+        		}
+        		default:
+        			if (stack.isEmpty() || !isValid((char)(int)stack.pop(), c)) return false;
+        	}
+        }
+        
+        return stack.isEmpty();
+    }
+	
+	private boolean isValid(char c1, char c2) {
+		return (c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}'); 
+	}
+	
+	public static void main(String[] args) {
+		String[] testCases = {"[", "]]", "((", "()[]{}", "([)]", "(]", "()" };
+		
+		ValidParentheses o = new ValidParentheses();
+		
+		for (String testCase : testCases) {
+			System.out.println(o.isValid(testCase));
+		}
+	}
+}
