7f672efc69c9ecbc307bb65b877e4cf675b33d83
John Qiao <qiaoshun8888@gmail.com>
2014-01-20 22:04:22 -0500

    Two Numbers
    	/**
    	 * Find two numbers in an array whose sum is x
    	 * 
    	 * Let A be an array of random integers and a given integer x. Find all
    	 * pairs of numbers from the array whose sum is equal to x.
    	 * 
    	 * 
    	 * The problem has different versions according to different
    conditions,
    	 * including:
    	 * 
    	 * - Unsorted array with O(1) space 
    	 * - Sorted array with O(1) space 
    	 * - Using extra space
    	 * 
    	 * */
    
    	/**
    	 * 1. The Brute Method.
    	 * */
    
    	/**
    	 * 2. O(n) Method for Sorted Array.
    	 * */
    	
    	/**
    	 * 3. O(n) Method using Hash Table. 
    	 * */

---
+++ src/amazon/FindTwoNumbersInArray.java
@@ -0,0 +1,32 @@
+package amazon;
+
+public class FindTwoNumbersInArray {
+
+	/**
+	 * Find two numbers in an array whose sum is x
+	 * 
+	 * Let A be an array of random integers and a given integer x. Find all
+	 * pairs of numbers from the array whose sum is equal to x.
+	 * 
+	 * 
+	 * The problem has different versions according to different conditions,
+	 * including:
+	 * 
+	 * - Unsorted array with O(1) space 
+	 * - Sorted array with O(1) space 
+	 * - Using extra space
+	 * 
+	 * */
+
+	/**
+	 * 1. The Brute Method.
+	 * */
+
+	/**
+	 * 2. O(n) Method for Sorted Array.
+	 * */
+	
+	/**
+	 * 3. O(n) Method using Hash Table. 
+	 * */
+}
