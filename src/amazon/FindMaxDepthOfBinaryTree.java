b880116c19fe2a60a0bf6d693d547fdce0b2cea2
John Qiao <qiaoshun8888@gmail.com>
2014-01-21 15:01:12 -0500

    Find maximum depth of a tree (No Recursion)

--- src/amazon/FindMaxDepthOfBinaryTree.java
+++ src/amazon/FindMaxDepthOfBinaryTree.java
@@ -1,5 +1,6 @@
 package amazon;
 
+import java.util.*;
 import myUtils.TreeUtils;
 import myUtils.datastructure.TreeNode;
 
@@ -19,6 +20,35 @@ public class FindMaxDepthOfBinaryTree {
 		return Math.max(depthHelper(root.left, height + 1), depthHelper(root.right, height + 1));
 	}
 	
+	public static void depthNoRecursion(TreeNode root) {
+		Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+		Stack<Integer> depthStack = new Stack<Integer>();
+		
+		nodeStack.push(root);
+		depthStack.push(1);
+		
+		int maxDepth = 0, minDepth = Integer.MAX_VALUE;
+		
+		while (!nodeStack.isEmpty()) {
+			TreeNode node = nodeStack.pop();
+			int depth = depthStack.pop();
+			
+			if (node.right != null) {
+				nodeStack.push(node.right);
+				depthStack.push(depth + 1);
+			}
+			if (node.left != null) {
+				nodeStack.push(node.left);
+				depthStack.push(depth + 1);
+			}
+			
+			if (depth > maxDepth) maxDepth = depth;
+			if (node.right == null && node.left == null && depth < minDepth) minDepth = depth;
+		}
+		
+		System.out.println("maxDepth: " + maxDepth + "\tminDepth: " + minDepth);
+	}
+	
 	public static void main(String [] args) {
 		/**
 		 * 		   3
@@ -33,5 +63,6 @@ public class FindMaxDepthOfBinaryTree {
 		int [] vs = {-1, 3, 3, 2, -1, -1, -1, 5, -1, -1, -1, -1, -1, -1, -1, 2};
 		TreeNode root = TreeUtils.generateBinaryTree(vs);
 		System.out.println("Height is: " + depth(root));
+		depthNoRecursion(root);
 	}
 }
