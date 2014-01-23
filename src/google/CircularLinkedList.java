c1293a26365f016ca8f14c6dcbc050fa5c1c49dd
qiaoshun8888 <qiaoshun8888@gmail.com>
2014-01-22 21:38:02 -0500

    Updates

--- src/google/CircularLinkedList.java
+++ src/google/CircularLinkedList.java
@@ -24,6 +24,24 @@ public class CircularLinkedList {
 		return false;
 	}
 	
+	public static LinkedNode findCircularNode(LinkedNode head) {
+		if (head == null) return null;
+		LinkedNode slowRunner = head, fastRunner = head;
+		while (fastRunner.next != null && fastRunner.next.next != null) {
+			slowRunner = slowRunner.next;
+			fastRunner = fastRunner.next.next;
+			if (slowRunner == fastRunner) break;
+		}
+		
+		slowRunner = head;
+		while (slowRunner != fastRunner) {
+			slowRunner = slowRunner.next;
+			fastRunner = fastRunner.next;
+		}
+		
+		return slowRunner;
+	}
+	
 	public static void main(String[] args) {
 		int [] vs = {1, 2, 3, 4, 5, 6, 7};
 		LinkedNode headC = LinkedListUtils.generateCircularLinkedList(vs, 3);
@@ -31,6 +49,7 @@ public class CircularLinkedList {
 		
 		System.out.println("Is circular linked list? " + isCircularLinkedList(headC));
 		LinkedListUtils.print(headC);
+		System.out.println("Circular Node is: " + findCircularNode(headC));
 		
 		System.out.println();
 		
--- src/myUtils/datastructure/LinkedNode.java
+++ src/myUtils/datastructure/LinkedNode.java
@@ -1,9 +1,16 @@
 package myUtils.datastructure;
 
 public class LinkedNode {
+	
 	public LinkedNode next;
 	public int val;
+	
 	public LinkedNode(int val) {
 		this.val = val;
+	}
+	
+	@Override
+	public String toString() {
+		return "Node(" + val + ")";
 	}
 }
