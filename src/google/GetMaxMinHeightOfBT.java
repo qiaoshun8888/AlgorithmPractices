225b90d69596b401dafb050a10ebd028a4531f8e
qiaoshun8888 <qiaoshun8888@gmail.com>
2014-01-25 11:31:18 -0500

    Max & min height of BT

---
+++ src/google/GetMaxMinHeightOfBT.java
@@ -0,0 +1,43 @@
+package google;
+
+import myUtils.TreeUtils;
+import myUtils.datastructure.TreeNode;
+
+/**
+ *	GetMaxMinHeightOfBT.java
+ *
+ *	Description: 
+ *
+ *	@author John Qiao <qiaoshun8888@gmail.com>
+ *	Date: Jan 24, 2014
+ */
+
+public class GetMaxMinHeightOfBT {
+
+	private static int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+	
+	public static void getMinMaxHeight(TreeNode root) {
+		getMinMaxHeightHelper(root, 1);
+	}
+	private static void getMinMaxHeightHelper(TreeNode node, int level) {
+		if (isLeaf(node)) {
+			min = Math.min(min, level);
+			max = Math.max(max, level);
+			return;
+		}
+		getMinMaxHeightHelper(node.left, level + 1);
+		getMinMaxHeightHelper(node.right, level + 1);
+	}
+	private static boolean isLeaf(TreeNode node) {
+		return node.left == null && node.right == null;
+	}
+	
+	public static void main(String[] args) {
+		int [] vs = {-1, 1, 2, 3, 4, 5, -1, -1, 1, 2, 3, 4, -1, -1, -1, -1};
+		TreeNode root = TreeUtils.generateBinaryTree(vs);
+		getMinMaxHeight(root);
+		TreeUtils.print(root);
+		System.out.println();
+		System.out.println("min: " + min + "  max: " + max);
+	}
+}
