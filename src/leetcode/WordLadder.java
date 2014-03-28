ca734276d1892b5740e4d8411661a3ab7ccd5962
John Qiao <qiaoshun8888@gmail.com>
2014-03-28 12:58:57 -0400

    minor updates

--- src/leetcode/WordLadder.java
+++ src/leetcode/WordLadder.java
@@ -11,18 +11,18 @@ public class WordLadder {
 	 * Only one letter can be changed at a time Each intermediate word must
 	 * exist in the dictionary For example,
 	 * 
-	 * Given: 
+	 * Given:
 	 * 
-	 * 		start = "hit" 
+	 * start = "hit"
 	 * 
-	 * 		end = "cog" 
+	 * end = "cog"
 	 * 
-	 * 		dict = ["hot","dot","dog","lot","log"]
+	 * dict = ["hot","dot","dog","lot","log"]
 	 * 
 	 * 
-	 * As one shortest transformation is 
+	 * As one shortest transformation is
 	 * 
-	 * 		"hit" -> "hot" -> "dot" -> "dog" -> "cog", 
+	 * "hit" -> "hot" -> "dot" -> "dog" -> "cog",
 	 * 
 	 * return its length 5.
 	 * 
@@ -30,27 +30,45 @@ public class WordLadder {
 	 * have the same length. All words contain only lowercase alphabetic
 	 * characters.
 	 * 
+	 * Solution:
+	 * 
+	 * Obviously, this problem should be solved by breadth first search (BFS).
+	 * We search from the start node, expand it to other node whenever possible.
+	 * 
+	 * "One way is for every node visiting, enumerates every word in dictionary
+	 * to see if the distance between two words are less than one. Suppose word
+	 * length is l, number of words in dictionary is n, then the time complexity
+	 * for this process is O(l*n). However, we could think of another way
+	 * leveraging the fast look up speed of dictionary. For current word,
+	 * enumerate every possible words within one distance from it and see if the
+	 * word in dictionary or not. The time complexity of this process is O(l *
+	 * 26). When number of words in dictionary is very large, the latter way is
+	 * much faster." --- http://zhaohongze.com/wordpress/2013/12/11/leetcode-word-ladder/
+	 * 
 	 * */
-	
+
 	public int ladderLength(String start, String end, HashSet<String> dict) {
-		if (start == null || start.length() == 0 || dict == null || dict.size() == 0) return 0;
-		
+		if (start == null || start.length() == 0 || dict == null
+				|| dict.size() == 0)
+			return 0;
+
 		dict.add(end);
-		
+
 		LinkedList<String> wordQueue = new LinkedList<String>();
 		LinkedList<Integer> distanceQueue = new LinkedList<Integer>();
-		
+
 		wordQueue.add(start);
 		distanceQueue.add(1);
-		
-		while(!wordQueue.isEmpty()) {
+
+		while (!wordQueue.isEmpty()) {
 			String word = wordQueue.pop();
 			int distance = distanceQueue.pop();
-			
-			if (word.equals(end)) return distance;
-			
+
+			if (word.equals(end))
+				return distance;
+
 			for (int i = 0; i < word.length(); i++) {
-				char [] cc = word.toCharArray();
+				char[] cc = word.toCharArray();
 				for (char c = 'a'; c <= 'z'; c++) {
 					cc[i] = c;
 					String temp = new String(cc);
@@ -62,22 +80,22 @@ public class WordLadder {
 				}
 			}
 		}
-		
+
 		return 0;
 	}
-	
+
 	public static void main(String[] args) {
 		WordLadder o = new WordLadder();
-		
+
 		HashSet<String> dict = new HashSet<String>();
 		dict.add("hot");
 		dict.add("dot");
 		dict.add("dog");
 		dict.add("lot");
 		dict.add("log");
-		
+
 		String start = "hit", end = "cog";
-		
+
 		System.out.println(o.ladderLength(start, end, dict));
 	}
 }
